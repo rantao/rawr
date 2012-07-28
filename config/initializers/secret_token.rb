41c5db3e097328122285f5da1fd5e2e9444eec4b
Ran Tao <ran@ephexi.com>
2012-07-27 17:47:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rawr::Application.config.secret_token = '8da795bdf246c989e00a7d97f027570f34bf5c9f4f161aed7dedd11b5d0d86f3a887342175d58066bb7d36af01833392f49a2758bac8412841c26eacd53faf24'
